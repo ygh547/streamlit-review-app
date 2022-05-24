cc3cc8df06e5238aeb85bb68350a78a0320d1c32
ygh547 <ygh547@naver.com>
2022-05-24 12:59:50 +0900

    add

--- app_eda.py
+++ app_eda.py
@@ -7,3 +7,11 @@ def run_eda() :
     df = pd.read_csv('data/review2.csv', index_col=0)
 
     st.dataframe(df)
+
+    st.text('리뷰의 길이별 히스토그램')
+
+    fig1 = plt.figure()
+    df['length'].hist()
+    st.pyplot(fig1)
+
+    st.text('리뷰의 길이가 가장 긴 데이터')
