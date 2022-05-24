4da3ce2861dfe5fd007dda7b032cdd101a97aee1
ygh547 <ygh547@naver.com>
2022-05-24 14:46:58 +0900

    add

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import string
 from app_eda import run_eda
 from app_home import run_home
 from app_ml import run_ml
+import nltk
+import numpy as np
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+my_stopwords = stopwords.words('english')
 
 def main():
     st.title('리뷰 긍정 부정 예측 맵')
@@ -17,6 +23,15 @@ def main():
     elif choice == menu[2]:
         run_ml()
 
+def message_cleaning(sentence) :
+  # 1. 구두점 제거
+  Test_punc_removed = [char for char in sentence if char not in string.punctuation]
+  # 2. 각 글자들을 하나의 문자열로 합친다.
+  Test_punc_removed_join = ''.join(Test_punc_removed)
+  # 3. 문장열에 불용어가 포함되어있는지 확인해서, 불용어 제거한다.
+  Test_punc_removed_join_clean = [word for word in Test_punc_removed_join.split() if word.lower() not in my_stopwords]
+  # 4. 결과로 남은 단어들만 리턴한다.
+  return Test_punc_removed_join_clean
 
 if __name__ == '__main__':
     main()
--- app_ml.py
+++ app_ml.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import string
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-my_stopwords = stopwords.words('english')
+import numpy as np
+import joblib
+
+
+
+                      
 
 def run_ml():
-    pass
+    st.subheader('문장을 입력하면, 긍정 부정 예측해 준다.')
+
+    sentence = st.text_input('문장 입력')
+
+    # 유저가 버튼을 누르면, 예측하도록 만든다.
+    if st.button('예측 실행'):
+
+
+        classifier = joblib.load('data/classifier.pkl')
+        vec = joblib.load('data/vec.pkl')
+        new_data = np.array([sentence])
+        X_new = vec.transform(new_data)
+        X_new = X_new.toarray()
+        y_pred = classifier.predict(X_new)
+        if y_pred[0] == 5 :
+            st.text('입력하신 문장은 긍정입니다.')
+        else :
+            st.text('입력하신 문장은 부정입니다.')
+        # st.write(y_pred)
 
