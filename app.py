07bb7e85609fa7b943b660c77e61abf76c3b468d
ygh547 <ygh547@naver.com>
2022-05-24 12:44:52 +0900

    add

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
 def main():
-    pass
+    st.title('리뷰 긍정 부정 예측 맵')
+
+    menu = ['Home','EDA',"ML"]
+
+    choice = st.sidebar.selectbox('메뉴 선택',menu)
+
+    if choice == menu[0]:
+        pass
+    elif choice == menu[1]:
+        pass
+    elif choice == menu[2]:
+        pass
+
 
 if __name__ == '__main__':
     main()
---
+++ app_eda.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def run_eda() :
+    pass
+
