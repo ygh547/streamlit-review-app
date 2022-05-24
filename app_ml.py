5c57600b389011b041e4a2fef85ec6a99fd3a8be
ygh547 <ygh547@naver.com>
2022-05-24 14:25:16 +0900

    add

---
+++ app_ml.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import string
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+my_stopwords = stopwords.words('english')
+
+def run_ml():
+    pass
+
